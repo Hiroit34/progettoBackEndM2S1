8c8cf8337b2f59e95218c94b667174877dc2ca0b
Hiroit34 <franz.natavio@gmail.com>
2024-05-17 10:39:51 +0200

    Inizio progetto.

---
+++ src/main/java/com/progetto/progettobackendm1s2/ProgettoBackEndM1S2Application.java
@@ -0,0 +1,13 @@
+package com.progetto.progettobackendm1s2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgettoBackEndM1S2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProgettoBackEndM1S2Application.class, args);
+    }
+
+}
